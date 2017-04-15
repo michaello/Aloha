c26578f5ee90fc5c11e45ff1cec4aad874d93cdc
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-04-15 14:43:39 +0200

    Initial Commit

---
+++ AlohaGIF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlohaGIF
+//
+//  Created by Michal Pyrka on 15/04/2017.
+//  Copyright © 2017 Michal Pyrka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
