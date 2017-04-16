0747118cc999103f8258f2643231c54ef0a4d1ec
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-04-16 18:39:31 +0200

    Added permissions for speech recognizer and Promise library for chaining permissions.

---
+++ AlohaGIF/Promise/Promise+Extras.swift
@@ -0,0 +1,162 @@
+//
+//  Promise+Extras.swift
+//  Promise
+//
+//  Created by Soroush Khanlou on 8/3/16.
+//
+//
+
+import Foundation
+
+struct PromiseCheckError: Error { }
+
+extension Promise {
+    /// Wait for all the promises you give it to fulfill, and once they have, fulfill itself
+    /// with the array of all fulfilled values.
+    public static func all<T>(_ promises: [Promise<T>]) -> Promise<[T]> {
+        return Promise<[T]>(work: { fulfill, reject in
+            guard !promises.isEmpty else { fulfill([]); return }
+            for promise in promises {
+                promise.then({ value in
+                    if !promises.contains(where: { $0.isRejected || $0.isPending }) {
+                        fulfill(promises.flatMap({ $0.value }))
+                    }
+                }).catch({ error in
+                    reject(error)
+                })
+            }
+        })
+    }
+
+    /// Resolves itself after some delay.
+    /// - parameter delay: In seconds
+    public static func delay(_ delay: TimeInterval) -> Promise<()> {
+        return Promise<()>(work: { fulfill, reject in
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                fulfill(())
+            })
+        })
+    }
+
+    /// This promise will be rejected after a delay.
+    public static func timeout<T>(_ timeout: TimeInterval) -> Promise<T> {
+        return Promise<T>(work: { fulfill, reject in
+            delay(timeout).then({ _ in
+                reject(NSError(domain: "com.khanlou.Promise", code: -1111, userInfo: [ NSLocalizedDescriptionKey: "Timed out" ]))
+            })
+        })
+    }
+
+    /// Fulfills or rejects with the first promise that completes
+    /// (as opposed to waiting for all of them, like `.all()` does).
+    public static func race<T>(_ promises: [Promise<T>]) -> Promise<T> {
+        return Promise<T>(work: { fulfill, reject in
+            guard !promises.isEmpty else { fatalError() }
+            for promise in promises {
+                promise.then(fulfill, reject)
+            }
+        })
+    }
+    public func addTimeout(_ timeout: TimeInterval) -> Promise<Value> {
+        return Promise.race(Array([self, Promise<Value>.timeout(timeout)]))
+    }
+
+    @discardableResult
+    public func always(on queue: ExecutionContext = DispatchQueue.main, _ onComplete: @escaping () -> ()) -> Promise<Value> {
+        return then(on: queue, { _ in
+            onComplete()
+            }, { _ in
+                onComplete()
+        })
+    }
+    
+    public func recover(_ recovery: @escaping (Error) throws -> Promise<Value>) -> Promise<Value> {
+        return Promise(work: { fulfill, reject in
+            self.then(fulfill).catch({ error in
+                do {
+                    try recovery(error).then(fulfill, reject)
+                } catch (let error) {
+                    reject(error)
+                }
+            })
+        })
+    }
+
+    public func ensure(_ check: @escaping (Value) -> Bool) -> Promise<Value> {
+        return self.then({ (value: Value) -> Value in
+            guard check(value) else {
+                throw PromiseCheckError()
+            }
+            return value
+        })
+    }
+
+
+    public static func retry<T>(count: Int, delay: TimeInterval, generate: @escaping () -> Promise<T>) -> Promise<T> {
+        if count <= 0 {
+            return generate()
+        }
+        return Promise<T>(work: { fulfill, reject in
+            generate().recover({ error in
+                return self.delay(delay).then({
+                    return retry(count: count-1, delay: delay, generate: generate)
+                })
+            }).then(fulfill).catch(reject)
+        })
+    }
+
+    @available(*, deprecated, message: "Use Promises.zip instead")
+    public static func zip<T, U>(_ first: Promise<T>, and second: Promise<U>) -> Promise<(T, U)> {
+        return Promises.zip(first, second)
+    }
+}
+
+public enum Promises {
+    public static func zip<T, U>(_ first: Promise<T>, _ second: Promise<U>) -> Promise<(T, U)> {
+        return Promise<(T, U)>(work: { fulfill, reject in
+            let resolver: (Any) -> () = { _ in
+                if let firstValue = first.value, let secondValue = second.value {
+                    fulfill((firstValue, secondValue))
+                }
+            }
+            first.then(resolver, reject)
+            second.then(resolver, reject)
+        })
+    }
+
+    // The following zip functions have been created with the 
+    // "Zip Functions Generator" playground page. If you need variants with
+    // more parameters, use it to generate them.
+
+    /// Zips 3 promises of different types into a single Promise whose
+    /// type is a tuple of 3 elements.
+    public static func zip<T1, T2, T3>(_ p1: Promise<T1>, _ p2: Promise<T2>, _ last: Promise<T3>) -> Promise<(T1, T2, T3)> {
+        return Promise<(T1, T2, T3)>(work: { (fulfill: @escaping ((T1, T2, T3)) -> Void, reject: @escaping (Error) -> Void) in
+            let zipped: Promise<(T1, T2)> = zip(p1, p2)
+
+            func resolver() -> Void {
+                if let zippedValue = zipped.value, let lastValue = last.value {
+                    fulfill((zippedValue.0, zippedValue.1, lastValue))
+                }
+            }
+            zipped.then({ _ in resolver() }, reject)
+            last.then({ _ in resolver() }, reject)
+        })
+    }
+
+    /// Zips 4 promises of different types into a single Promise whose
+    /// type is a tuple of 4 elements.
+    public static func zip<T1, T2, T3, T4>(_ p1: Promise<T1>, _ p2: Promise<T2>, _ p3: Promise<T3>, _ last: Promise<T4>) -> Promise<(T1, T2, T3, T4)> {
+        return Promise<(T1, T2, T3, T4)>(work: { (fulfill: @escaping ((T1, T2, T3, T4)) -> Void, reject: @escaping (Error) -> Void) in
+            let zipped: Promise<(T1, T2, T3)> = zip(p1, p2, p3)
+
+            func resolver() -> Void {
+                if let zippedValue = zipped.value, let lastValue = last.value {
+                    fulfill((zippedValue.0, zippedValue.1, zippedValue.2, lastValue))
+                }
+            }
+            zipped.then({ _ in resolver() }, reject)
+            last.then({ _ in resolver() }, reject)
+        })
+    }
+}
---
+++ AlohaGIF/Promise/Promise.swift
@@ -0,0 +1,273 @@
+//
+//  Promise.swift
+//  Promise
+//
+//  Created by Soroush Khanlou on 7/21/16.
+//
+//
+
+import Foundation
+
+public protocol ExecutionContext {
+    func execute(_ work: @escaping () -> Void)
+}
+
+extension DispatchQueue: ExecutionContext {
+    public func execute(_ work: @escaping () -> Void) {
+        self.async(execute: work)
+    }
+}
+
+public final class InvalidatableQueue: ExecutionContext {
+
+    private var valid = true
+
+    private let queue: DispatchQueue
+
+    public init(queue: DispatchQueue = .main) {
+        self.queue = queue
+    }
+
+    public func invalidate() {
+        valid = false
+    }
+
+    public func execute(_ work: @escaping () -> Void) {
+        guard valid else { return }
+        self.queue.async(execute: work)
+    }
+
+}
+
+struct Callback<Value> {
+    let onFulfilled: (Value) -> ()
+    let onRejected: (Error) -> ()
+    let queue: ExecutionContext
+    
+    func callFulfill(_ value: Value) {
+        queue.execute({
+            self.onFulfilled(value)
+        })
+    }
+    
+    func callReject(_ error: Error) {
+        queue.execute({
+            self.onRejected(error)
+        })
+    }
+}
+
+enum State<Value>: CustomStringConvertible {
+
+    /// The promise has not completed yet.
+    /// Will transition to either the `fulfilled` or `rejected` state.
+    case pending
+
+    /// The promise now has a value.
+    /// Will not transition to any other state.
+    case fulfilled(value: Value)
+
+    /// The promise failed with the included error.
+    /// Will not transition to any other state.
+    case rejected(error: Error)
+
+
+    var isPending: Bool {
+        if case .pending = self {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var isFulfilled: Bool {
+        if case .fulfilled = self {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var isRejected: Bool {
+        if case .rejected = self {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    var value: Value? {
+        if case let .fulfilled(value) = self {
+            return value
+        }
+        return nil
+    }
+    
+    var error: Error? {
+        if case let .rejected(error) = self {
+            return error
+        }
+        return nil
+    }
+
+
+    var description: String {
+        switch self {
+        case .fulfilled(let value):
+            return "Fulfilled (\(value))"
+        case .rejected(let error):
+            return "Rejected (\(error))"
+        case .pending:
+            return "Pending"
+        }
+    }
+}
+
+
+public final class Promise<Value> {
+    
+    private var state: State<Value>
+    private let lockQueue = DispatchQueue(label: "promise_lock_queue", qos: .userInitiated)
+    private var callbacks: [Callback<Value>] = []
+    
+    public init() {
+        state = .pending
+    }
+    
+    public init(value: Value) {
+        state = .fulfilled(value: value)
+    }
+    
+    public init(error: Error) {
+        state = .rejected(error: error)
+    }
+    
+    public convenience init(queue: DispatchQueue = DispatchQueue.global(qos: .userInitiated), work: @escaping (_ fulfill: @escaping (Value) -> (), _ reject: @escaping (Error) -> () ) throws -> ()) {
+        self.init()
+        queue.async(execute: {
+            do {
+                try work(self.fulfill, self.reject)
+            } catch let error {
+                self.reject(error)
+            }
+        })
+    }
+
+    /// - note: This one is "flatMap"
+    @discardableResult
+    public func then<NewValue>(on queue: ExecutionContext = DispatchQueue.main, _ onFulfilled: @escaping (Value) throws -> Promise<NewValue>) -> Promise<NewValue> {
+        return Promise<NewValue>(work: { fulfill, reject in
+            self.addCallbacks(
+                on: queue,
+                onFulfilled: { value in
+                    do {
+                        try onFulfilled(value).then(fulfill, reject)
+                    } catch let error {
+                        reject(error)
+                    }
+                },
+                onRejected: reject
+            )
+        })
+    }
+    
+    /// - note: This one is "map"
+    @discardableResult
+    public func then<NewValue>(on queue: ExecutionContext = DispatchQueue.main, _ onFulfilled: @escaping (Value) throws -> NewValue) -> Promise<NewValue> {
+        return then(on: queue, { (value) -> Promise<NewValue> in
+            do {
+                return Promise<NewValue>(value: try onFulfilled(value))
+            } catch let error {
+                return Promise<NewValue>(error: error)
+            }
+        })
+    }
+    
+    @discardableResult
+    public func then(on queue: ExecutionContext = DispatchQueue.main, _ onFulfilled: @escaping (Value) -> (), _ onRejected: @escaping (Error) -> () = { _ in }) -> Promise<Value> {
+        _ = Promise<Value>(work: { fulfill, reject in
+            self.addCallbacks(
+                on: queue,
+                onFulfilled: { value in
+                    fulfill(value)
+                    onFulfilled(value)
+                },
+                onRejected: { error in
+                    reject(error)
+                    onRejected(error)
+                }
+            )
+        })
+        return self
+    }
+    
+    @discardableResult
+    public func `catch`(on queue: ExecutionContext = DispatchQueue.main, _ onRejected: @escaping (Error) -> ()) -> Promise<Value> {
+        return then(on: queue, { _ in }, onRejected)
+    }
+    
+    public func reject(_ error: Error) {
+        updateState(.rejected(error: error))
+    }
+    
+    public func fulfill(_ value: Value) {
+        updateState(.fulfilled(value: value))
+    }
+    
+    public var isPending: Bool {
+        return !isFulfilled && !isRejected
+    }
+    
+    public var isFulfilled: Bool {
+        return value != nil
+    }
+    
+    public var isRejected: Bool {
+        return error != nil
+    }
+    
+    public var value: Value? {
+        return lockQueue.sync(execute: {
+            return self.state.value
+        })
+    }
+    
+    public var error: Error? {
+        return lockQueue.sync(execute: {
+            return self.state.error
+        })
+    }
+    
+    private func updateState(_ state: State<Value>) {
+        guard self.isPending else { return }
+        lockQueue.sync(execute: {
+            self.state = state
+        })
+        fireCallbacksIfCompleted()
+    }
+    
+    private func addCallbacks(on queue: ExecutionContext = DispatchQueue.main, onFulfilled: @escaping (Value) -> (), onRejected: @escaping (Error) -> ()) {
+        let callback = Callback(onFulfilled: onFulfilled, onRejected: onRejected, queue: queue)
+        lockQueue.async(execute: {
+            self.callbacks.append(callback)
+        })
+        fireCallbacksIfCompleted()
+    }
+    
+    private func fireCallbacksIfCompleted() {
+        lockQueue.async(execute: {
+            guard !self.state.isPending else { return }
+            self.callbacks.forEach { callback in
+                switch self.state {
+                case let .fulfilled(value):
+                    callback.callFulfill(value)
+                case let .rejected(error):
+                    callback.callReject(error)
+                default:
+                    break
+                }
+            }
+            self.callbacks.removeAll()
+        })
+    }
+}
