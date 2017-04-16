83b169e52065283e272f38f19766d59d1a30d3fe
Michal Pyrka <michal.pyrka@droidsonroids.pl>
2017-04-16 23:23:59 +0200

    Simple camera logic added.

--- AlohaGIF/CameraViewController.swift
+++ AlohaGIF/CameraViewController.swift
@@ -7,29 +7,108 @@
 //
 
 import UIKit
+import AVFoundation
 
-class CameraViewController: UIViewController {
+final class CameraViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var previewView: PreviewView!
+    var recordButton: RecordButton!
+    
+    private struct Constants {
+        static let allPossibleCameras: [(cameraType: AVCaptureDeviceType, position: AVCaptureDevicePosition)] = [
+(AVCaptureDeviceType.builtInDualCamera, AVCaptureDevicePosition.back),
+(AVCaptureDeviceType.builtInWideAngleCamera, AVCaptureDevicePosition.back),
+(AVCaptureDeviceType.builtInWideAngleCamera ,AVCaptureDevicePosition.front)
+        ]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private let session = AVCaptureSession()
+    private let sessionQueue = DispatchQueue(label: "sessionQueue", attributes: [], target: nil)
+    private let sessionPresetQuality = AVCaptureSessionPresetHigh
+    private let movieFileOutput = AVCaptureMovieFileOutput()
+    private var videoDeviceInput: AVCaptureDeviceInput?
+    var anyCamera: AVCaptureDevice? {
+        return Constants.allPossibleCameras.flatMap { AVCaptureDevice.defaultDevice(withDeviceType: $0.cameraType, mediaType: AVMediaTypeVideo, position: $0.position) }.first
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRecordButton()
+        previewView.session = session
+        checkAuthorization()
+        sessionQueue.async { [unowned self] in
+            self.configureSession()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        session.startRunning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        session.stopRunning()
+    }
+    
+    private func addVideoInput() {
+        guard let anyCamera = anyCamera, let videoDeviceInput = try? AVCaptureDeviceInput(device: anyCamera) else { return }
+        if session.canAddInput(videoDeviceInput) {
+            session.addInput(videoDeviceInput)
+            self.videoDeviceInput = videoDeviceInput
+            DispatchQueue.main.async {
+                self.previewView.videoPreviewLayer.connection.videoOrientation = .portrait
+            }
+        }
+    }
+    
+    private func addAudioInput() {
+        let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+        let audioDeviceInput = try? AVCaptureDeviceInput(device: audioDevice)
+        
+        if let audioDeviceInput = audioDeviceInput, session.canAddInput(audioDeviceInput) {
+            session.addInput(audioDeviceInput)
+        }
+    }
+    
+    private func configureSession() {
+        addVideoInput()
+        addAudioInput()
 
+        if session.canAddOutput(movieFileOutput) {
+            session.beginConfiguration()
+            session.addOutput(movieFileOutput)
+            session.sessionPreset = sessionPresetQuality
+            if let connection = movieFileOutput.connection(withMediaType: AVMediaTypeVideo) {
+                if connection.isVideoStabilizationSupported {
+                    connection.preferredVideoStabilizationMode = .auto
+                }
+            }
+            session.commitConfiguration()
+        }
+    }
+    
+    private func checkAuthorization() {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .notDetermined:
+            requestForCameraAccess()
+        default: ()
+        }
+    }
+    
+    private func requestForCameraAccess() {
+        sessionQueue.suspend()
+        AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { [unowned self] isGranted in
+            self.sessionQueue.async { [unowned self] in
+                self.configureSession()
+            }
+            self.sessionQueue.resume()
+        })
+    }
+    
+    private func setupRecordButton() {
+        recordButton = RecordButton(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+        recordButton.center = self.view.center
+        view.addSubview(recordButton)
+    }
 }
--- AlohaGIF/RecordButton.swift
+++ AlohaGIF/RecordButton.swift
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Samuel Beek. All rights reserved.
 //
 
+import UIKit
+
 @objc public enum RecordButtonState : Int {
     case recording, idle, hidden;
 }
