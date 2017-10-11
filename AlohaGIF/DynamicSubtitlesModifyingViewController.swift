1419c60709ae0800c46adfd5ae8a1762ac863681
Michał Pyrka <majkpe@gmail.com>
2017-10-11 22:34:56 +0200

    Recovered DynamicSubtitlesModifyingViewController

---
+++ AlohaGIF/DynamicSubtitlesModifyingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DynamicSubtitlesModifyingViewController.swift
+//  AlohaGIF
+//
+//  Created by Michal Pyrka on 11/10/2017.
+//  Copyright © 2017 Michal Pyrka. All rights reserved.
+//
+
+import UIKit
+
+class DynamicSubtitlesModifyingViewController: UIViewController {
+    weak var handler: ModifiedDynamicSubtitlesHandler?
+}
